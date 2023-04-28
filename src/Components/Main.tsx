bea54f824af4cd338d0ae8ef4f58f1e925bb764e
Kaue Graciano <kauegraciano93@gmail.com>
2023-04-28 00:21:13 -0300

    Feita as primeiras configurações da requisição

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -2,14 +2,26 @@ import React, { useState } from 'react'
 
 import styles from './Main.module.css';
 
+import axios from 'axios';
+
 export const Main = ()=>{
 
     const [city, setCity] = useState('');
 
     const handleInput = (event: React.ChangeEvent<HTMLInputElement>) =>{ 
         setCity(event.target.value);
-      }
-     
+    }
+
+    const apiKey = "56817c46f017148ea21e7d189c4e7f8b";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+    
+    const getInfo = ()=>{
+        axios.get(url)
+        .then(response =>{
+            const data = response.data;
+            console.log(data);
+        })
+    }
     
     return (
       <div className={styles.main}>
@@ -19,7 +31,10 @@ export const Main = ()=>{
 
             <div className={styles.searchBar}>
                 <input type="text" id={styles.searchBox} placeholder="Insira o nome da sua cidade" value={city} onChange={handleInput} />
+                <button className={styles.searchButton} onClick={getInfo}>Buscar</button>
             </div>
+
+
         </div>
         
         <div className={styles.containerCard}>
